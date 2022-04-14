c44d7fc640dc791bd8fc776626ff14059a4f68ac
unknown <taha.iu.bd@gmail.com>
2022-04-15 00:16:43 +0600

    footer-add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
 import NavBar from './components/NavBar/NavBar'
 
 function App() {
     return (
-        <div className="App">
+        <div>
             <NavBar></NavBar>
+            <Footer></Footer>
         </div>
     )
 }
